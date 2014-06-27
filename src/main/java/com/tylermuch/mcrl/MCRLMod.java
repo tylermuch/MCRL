23aad54331aa37aad00fa918eb0c16b97a7c2665
Tyler Much <tylermuch@gmail.com>
2014-06-27 15:36:54 -0500

    Mod skeleton in place

--- src/main/java/com/tylermuch/mcrl/MCRLMod.java
+++ src/main/java/com/tylermuch/mcrl/MCRLMod.java
@@ -1,7 +1,41 @@
 package com.tylermuch.mcrl;
 
-/**
- * Created by tylermuch on 6/27/14.
- */
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStoppingEvent;
+import jssc.SerialPort;
+
+@Mod(modid = MCRLMod.MODID, version = MCRLMod.VERSION)
 public class MCRLMod {
+
+    public static final String MODID = "mcrl";
+    public static final String VERSION = "1.0";
+
+    @Mod.Instance(MCRLMod.MODID)
+    public static MCRLMod instance;
+
+    public static SerialPort serial = null;
+
+    @Mod.EventHandler
+    public void preInit (FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init (FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit (FMLPostInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void stopping (FMLServerStoppingEvent event) {
+        
+    }
+
 }
